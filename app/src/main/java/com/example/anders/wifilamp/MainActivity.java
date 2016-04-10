77a4aed2a7da23742c21c5794cf9bfafa7e850e4
Anders Sjobom <anders.sjobom@live.se>
2016-04-10 16:42:54 +0200

    Added INTERNET Permission

--- app/src/main/java/com/example/anders/wifilamp/MainActivity.java
+++ app/src/main/java/com/example/anders/wifilamp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.anders.wifilamp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -21,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void ledOn (View v){
+        Log.d("BEFORE REQUEST", "");
         lampRequest("on");
     }
 
@@ -31,12 +33,13 @@ public class MainActivity extends AppCompatActivity {
     public void lampRequest (String action){
         String url;
         if (action.equals("on")){ url = "http://192.168.1.100/LED=ON/"; }
-        else if(action.equals("off")){ url = "http://192.168.1.100/LED=ON/"; }
+        else if(action.equals("off")){ url = "http://192.168.1.100/LED=OFF/"; }
         else{ return; }
         RequestQueue queue = Volley.newRequestQueue(this);
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
+                Log.d("REPLY", response);
                 TextView stateText = (TextView) findViewById(R.id.stateText);
                 if (response.indexOf("On<br><br>") != -1){
                     stateText.setText("LED is ON!");
@@ -49,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
         new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-
+                Log.d("ERROR", error.toString());
             }
         });
         queue.add(stringRequest);
